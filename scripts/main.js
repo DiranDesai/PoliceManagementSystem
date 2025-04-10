ba8a3b09acdf5e8ad682ce1b9d9522e9a9e3314e
DiranDesai <dirantechie@gmail.com>
2025-04-10 04:31:13 -0700

    First commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,30 @@
 const logo = document.querySelector(".logo")
 const officerProfile = document.querySelector(".officer-profile")
 const signOutBtn = document.querySelector(".signout-btn")
+const loaderWrapper = document.querySelector(".loader-wrapper")
 
 
 
 officerProfile.addEventListener("click", () => {
+    const data = showLoader()
+
+    console.log(data)
     window.location.href = "profile.html"
 })
 
 logo.addEventListener("click", () => {
+    showLoader()
     window.location.href = "index.html"
 })
 
 signOutBtn.addEventListener("click", () => {
     window.location.href = "login.html"
 })
+
+function showLoader() {
+    loaderWrapper.classList.remove("hidden")
+    setInterval(() => {
+        loaderWrapper.classList.add("hidden")
+        return true
+    }, 2000)
+}
